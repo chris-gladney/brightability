61818521724ce9e90f1e796aeb876faa3daf8300
Chris Gladney <christophergladney@hotmail.co.uk>
2025-05-02 14:41:38 +0100

    Links added for all navigation elements. Need tp complete links for all the service cards

---
+++ ba-fe/src/components/PhotoAlbum.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+function PhotoAlbum() {
+  return (
+    <>
+      <Header />
+      
+      <Footer />
+    </>
+  );
+}
+
+export default PhotoAlbum;
---
+++ ba-fe/src/components/Staff.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Staff() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Staff;
