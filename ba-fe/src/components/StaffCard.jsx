a3db66b9a22c5fc2df9f1b30524e30fc37886926
Chris Gladney <christophergladney@hotmail.co.uk>
2025-05-03 20:37:52 +0100

    Staff page styling complete

--- ba-fe/src/components/StaffCard.jsx
+++ ba-fe/src/components/StaffCard.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import placeholderImg from "../assets/placeholderImg.png";
 
 function StaffCard({ name, job_title, description }) {
+  const [infoToDisplay, setinfoToDisplay] = useState("description");
+
   return (
-    <div className="staff-card">
-      <div className="staff-description">
+    <div
+      className="staff-card"
+      onMouseOver={() => {
+        setinfoToDisplay("image");
+      }}
+      onMouseOut={() => {
+        setinfoToDisplay("description");
+      }}
+    >
+      <div
+        className={`staff-img ${
+          infoToDisplay !== "image" ? "hidden-staff-info" : ""
+        }`}
+      ></div>
+      <div
+        className={`staff-description ${
+          infoToDisplay !== "description" ? "hidden-staff-info" : ""
+        }`}
+      >
         <h4>{name}</h4>
         <h5>{job_title}</h5>
         <p>{description}</p>
